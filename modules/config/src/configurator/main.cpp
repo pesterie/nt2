5ddf1500c4558b45eb7306b3af2ef83ed6a22c11
pesterie <pesterie@gmail.com>
2011-06-07 16:59:16 +0200

    Adding runtime detection functions (out of nt2 library for now)

--- modules/config/src/configurator/main.cpp
+++ modules/config/src/configurator/main.cpp
@@ -6,156 +6,94 @@
 ///                 See accompanying file LICENSE.txt or copy at
 ///                     http://www.boost.org/LICENSE_1_0.txt
 //////////////////////////////////////////////////////////////////////////////
+#include <boost/program_options.hpp>
+#include <boost/array.hpp>
+#include <iostream>
+#include <string>
+#include <nt2/sdk/config/configurator/cache.hpp>
+#include <nt2/sdk/config/configurator/cpuid.hpp>
+#include <nt2/sdk/config/configurator/utils.hpp>
+#include <nt2/sdk/config/configurator/core.hpp>
+#include <nt2/sdk/config/configurator/vectorial.hpp>
+#include <iterator>
+#include <cassert>
 
-#include <hwloc.h>
-#include <errno.h>
-#include <stdio.h>
-#include <string.h>
+namespace po = boost::program_options;
+using namespace std;
+void test(void);
 
-static void print_children(hwloc_topology_t topology, hwloc_obj_t obj, 
-                           int depth)
+////////////////////////////////////////////////////////////////////////////////
+// Entry point for the configurator
+// Documentation: http://nt2.lri.fr/sdk/configurator.html
+////////////////////////////////////////////////////////////////////////////////
+
+int main(int ac, char *av[])
 {
-    char string[128];
-    unsigned i;
+  try {
+    
+    po::options_description desc("Configurator options");
+    desc.add_options()
+      ("runtime_test", "Test your hardware specification")
+      ;
+    
+    po::variables_map vm;        
+    po::store(po::parse_command_line(ac, av, desc), vm);
+    po::notify(vm);    
 
-    hwloc_obj_snprintf(string, sizeof(string), topology, obj, "#", 0);
-    printf("%*s%s\n", 2*depth, "", string);
-    for (i = 0; i < obj->arity; i++) {
-        print_children(topology, obj->children[i], depth + 1);
+    if (vm.count("runtime_test")) {
+      test();
+      return 1;
     }
+    else {
+      cout << "Add option --runtime_test to test runtime configuration functions\n";
+    }
+  }
+  catch(exception& e) {
+    cerr << "error: " << e.what() << "\n";
+    return 1;
+  }
+  catch(...) {
+    cerr << "Exception of unknown type!\n";
+  }
+  
+  return 0;
 }
 
-int main(int argc, char *argv[])
+void test(void)
 {
-  int depth;
-  unsigned i, n;
-  unsigned long size;
-  int levels;
-  char string[128];
-  int topodepth;
-  hwloc_topology_t topology;
-  hwloc_cpuset_t cpuset;
-  hwloc_obj_t obj;
-  
-  /* Allocate and initialize topology object. */
-  hwloc_topology_init(&topology);
-  
-  /* Perform the topology detection. */
-  hwloc_topology_load(topology);
-  
-  /* Optionally, get some additional topology information
-     in case we need the topology depth later. */
-  topodepth = hwloc_topology_get_depth(topology);
-  
-  /*****************************************************************
-   * First example:
-   * Walk the topology with an array style, from level 0 (always
-   * the system level) to the lowest level (always the proc level).
-   *****************************************************************/
-  for (depth = 0; depth < topodepth; depth++) {
-    printf("*** Objects at level %d\n", depth);
-    for (i = 0; i < hwloc_get_nbobjs_by_depth(topology, depth); 
-         i++) {
-      hwloc_obj_snprintf(string, sizeof(string), topology,
-                         hwloc_get_obj_by_depth(topology, depth, i),
-                         "#", 0);
-      printf("Index %u: %s\n", i, string);
+  boost::array<std::string,10> ext = { {"mmx","sse","sse2","sse3","ssse3","sse4.1","sse4.2","avx","xop","fma4"} };
+
+  std::cout << "===============================" << std::endl;
+  std::cout << "Threads (logical cores per physical cores) : " << nt2::config::get_threads() << std::endl;
+  std::cout << "Logical Cores : " << nt2::config::get_logical_cores() << std::endl;
+  std::cout << "Cores : " << nt2::config::get_physical_cores() << std::endl;
+
+  nt2::config::cache_report cache;
+  nt2::config::get_cache_infos(cache);
+  int coh = nt2::config::get_cache_coherency_line_size(cache);
+
+  std::cout << "===============================" << std::endl;
+  std::cout << "Cache size : " << std::endl;
+  std::cout << "  L1 = : " << cache.L1 << std::endl;
+  std::cout << "  L2 = : " << cache.L2 << std::endl;
+  std::cout << "  L3 = : " << cache.L3 << std::endl;
+  std::cout << "Cache line size : " << std::endl;
+  std::cout << "  l1 = : " << cache.l1 << std::endl;
+  std::cout << "  l2 = : " << cache.l2 << std::endl;
+  std::cout << "  l3 = : " << cache.l3 << std::endl;
+  std::cout << "Cache coherency line size : " << nt2::config::get_cache_coherency_line_size(cache) << std::endl;
+  std::cout << "===============================" << std::endl;
+
+  for(int i = 0; i < 10; ++i)
+    {
+      if (nt2::config::has_vectorial_extension(ext[i]))
+        {
+          std::cout << ext[i] << " Found" << std::endl;
+        }
+      else
+        {
+          std::cout << ext[i] << " Not Found" << std::endl;
+        }
     }
-  }
-  
-  /*****************************************************************
-   * Second example:
-   * Walk the topology with a tree style.
-   *****************************************************************/
-  printf("*** Printing overall tree\n");
-  print_children(topology, hwloc_get_root_obj(topology), 0);
-  
-  /*****************************************************************
-   * Third example:
-   * Print the number of sockets.
-   *****************************************************************/
-  depth = hwloc_get_type_depth(topology, HWLOC_OBJ_SOCKET);
-  if (depth == HWLOC_TYPE_DEPTH_UNKNOWN) {
-    printf("*** The number of sockets is unknown\n");
-  } else {
-    printf("*** %u socket(s)\n", 
-           hwloc_get_nbobjs_by_depth(topology, depth));
-  }
-  
-  /*****************************************************************
-   * Fourth example:
-   * Compute the amount of cache that the first logical processor
-   * has above it.
-   *****************************************************************/
-  levels = 0;
-  size = 0;
-  for (obj = hwloc_get_obj_by_type(topology, HWLOC_OBJ_PU, 0);
-       obj;
-       obj = obj->parent)
-    if (obj->type == HWLOC_OBJ_CACHE) {
-      levels++;
-      size += obj->attr->cache.size;
-    }
-  printf("*** Logical processor 0 has %d caches totaling %luKB\n", 
-         levels, size / 1024);
-  
-  /*****************************************************************
-   * Fifth example:
-   * Bind to only one thread of the last core of the machine.
-   *
-   * First find out where cores are, or else smaller sets of CPUs if
-   * the OS doesn't have the notion of a "core".
-   *****************************************************************/
-  depth = hwloc_get_type_or_below_depth(topology, HWLOC_OBJ_CORE);
-  
-  /* Get last core. */
-  obj = hwloc_get_obj_by_depth(topology, depth,
-                               hwloc_get_nbobjs_by_depth(topology, depth) - 1);
-  if (obj) {
-    /* Get a copy of its cpuset that we may modify. */
-    cpuset = hwloc_bitmap_dup(obj->cpuset);
-    
-    /* Get only one logical processor (in case the core is
-       SMT/hyperthreaded). */
-    hwloc_bitmap_singlify(cpuset);
 
-    /* And try to bind ourself there. */
-    if (hwloc_set_cpubind(topology, cpuset, 0)) {
-      char *str;
-      int error = errno;
-      hwloc_bitmap_asprintf(&str, obj->cpuset);
-      printf("Couldn't bind to cpuset %s: %s\n", str, strerror(error));
-      free(str);
-    }
-        
-    /* Free our cpuset copy */
-    hwloc_bitmap_free(cpuset);
-  }
-
-  /*****************************************************************
-   * Sixth example:
-   * Allocate some memory on the last NUMA node, bind some existing
-   * memory to the last NUMA node.
-   *****************************************************************/
-  /* Get last node. */
-  n = hwloc_get_nbobjs_by_type(topology, HWLOC_OBJ_NODE);
-  if (n) {
-    void *m;
-    size = 1024*1024;
-
-    obj = hwloc_get_obj_by_type(topology, HWLOC_OBJ_NODE, n - 1);
-    m = hwloc_alloc_membind_nodeset(topology, size, obj->nodeset,
-                                    HWLOC_MEMBIND_DEFAULT, 0);
-    hwloc_free(topology, m, size);
-
-    m = malloc(size);
-    hwloc_set_area_membind_nodeset(topology, m, size, obj->nodeset,
-                                   HWLOC_MEMBIND_DEFAULT, 0);
-    free(m);
-  }
-
-  /* Destroy topology object. */
-  hwloc_topology_destroy(topology);
-
-  return 0;
 }
